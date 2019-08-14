5124a70f8273fadf19b01108f87eabf1277318f6
Kabelo MASHAO <kmashao@c5r1s9.wethinkcode.co.za>
2019-08-14 17:05:28 +0200

    some prog

---
+++ src/main/java/co/za/wethinkcode/swingy/hero/Hero.java
@@ -0,0 +1,97 @@
+package co.za.wethinkcode.swingy.hero;
+
+
+
+public class Hero implements HeroStats {
+
+	private String heroName;
+	private String heroClass;
+	private int level;
+	private int experience;
+	private int attack;
+	private int defense;
+	private int hp;
+
+	@Override
+	public void setName(String heroName) {
+		this.heroName = heroName;
+	}
+
+	/**
+	 * @return the heroName
+	 */
+	public String getHeroName() {
+		return heroName;
+	}
+
+	@Override
+	public void setClass(String heroClass) {
+		this.heroClass = heroClass;
+	}
+
+	/**
+	 * @return the heroClass
+	 */
+	public String getHeroClass() {
+		return heroClass;
+	}
+
+	@Override
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	/**
+	 * @return the level
+	 */
+	public int getHeroLevel() { return level; }
+
+	@Override
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	/**
+	 * @return the experience
+	 */
+	public int getExperience() {
+		return experience;
+	}
+
+	@Override
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	/**
+	 * @return the attack
+	 */
+	public int getAttack() {
+		return attack;
+	}
+
+	@Override
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	/**
+	 * @return the defense
+	 */
+	public int getDefense() {
+		return defense;
+	}
+
+	@Override
+	public void setHitPoints(int hp) {
+		this.hp = hp;
+	}
+
+	/**
+	 * @return the hp
+	 */
+	public int getHitPoints() {
+		return hp;
+	}
+
+}
---
+++ src/main/java/co/za/wethinkcode/swingy/hero/HeroCreator.java
@@ -0,0 +1,33 @@
+package co.za.wethinkcode.swingy.hero;
+
+public class HeroCreator {
+
+	private HeroBuild heroBuild;
+
+	public HeroCreator(HeroBuild heroBuild) {
+		this.heroBuild = heroBuild;
+	}
+
+	public Hero getHero() {
+		return this.heroBuild.getHero();
+	}
+
+	public void createHero() throws NumberFormatException {
+
+		//this.heroBuild.heroName();
+
+		this.heroBuild.heroClass();
+
+		this.heroBuild.heroLevel();
+
+		this.heroBuild.heroExperience();
+
+		this.heroBuild.heroAttack();
+
+		this.heroBuild.heroDefense();
+
+		this.heroBuild.heroHitPoints();
+
+	}
+
+}
